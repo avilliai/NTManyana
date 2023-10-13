c14e1d6576b2243dedea03cc1c7939597eaa904f
DEII <1840094972@qq.com>
2023-10-13 21:57:06 +0800

    小功能整合

--- run/singleModule.py
+++ run/singleModule.py
@@ -12,7 +12,7 @@ from pathlib import Path
 
 import httpx
 import yaml
-
+from plugins.historicalToday import hisToday as hisToday1
 from plugins import weatherQuery
 from plugins.RandomStr import random_str
 from plugins.arkOperator import arkOperator
@@ -55,11 +55,11 @@ qiancao=on_fullmatch("抽签")
 poetrys=on_fullmatch("诗经")
 YiChou=on_fullmatch("周易")
 screenShoot1=on_startswith("截图#")
-honorYou=on_startswith("奖状")
+honorYou=on_startswith("/奖状")
 ark10=on_fullmatch("方舟十连")
 starRailOrdor=on_fullmatch("星铁十连")
 #拿数据
-logger=newLogger()
+logger1=newLogger()
 with open('config/api.yaml', 'r', encoding='utf-8') as f:
     result121 = yaml.load(f.read(), Loader=yaml.FullLoader)
 api_KEY=result121.get("weatherXinZhi")
@@ -68,8 +68,8 @@ app_key=result121.get("youdao").get("app_key")
 nasa_api=result121.get("nasa_api")
 
 proxy=result121.get("proxy")
-logger.info("抽卡/运势模块启动")
-logger.info("额外的功能 启动完成")
+logger1.info("抽卡/运势模块启动")
+logger1.info("额外的功能 启动完成")
 with open("data/odes.json", encoding="utf-8") as fp:
     odes = json.loads(fp.read())
 with open("data/IChing.json", encoding="utf-8") as fp:
@@ -100,7 +100,7 @@ async def meme(bot: Bot, event: MessageEvent):
     global memeData
 
     try:
-        logger.info("使用网络meme")
+        logger1.info("使用网络meme")
 
         url = 'https://meme-api.com/gimme'
         proxies = {
@@ -109,7 +109,7 @@ async def meme(bot: Bot, event: MessageEvent):
         }
         async with httpx.AsyncClient(timeout=20) as client:
             r = await client.get(url)
-            logger.info(r.json().get("preview")[-1])
+            logger1.info(r.json().get("preview")[-1])
             async with httpx.AsyncClient(timeout=20, proxies=proxies) as client:
                 r = await client.get(r.json().get("preview")[-1])
                 img = Image1.open(BytesIO(r.content))  # 从二进制数据创建图片对象
@@ -119,10 +119,10 @@ async def meme(bot: Bot, event: MessageEvent):
 
 
     except:
-        logger.warning("网络meme出错，使用本地meme图")
+        logger1.warning("网络meme出错，使用本地meme图")
         la = os.listdir("data/pictures/meme")
         la = "data/pictures/meme/" + random.choice(la)
-        logger.info("掉落了一张meme图")
+        logger1.info("掉落了一张meme图")
         await bot.send_group_message(event.scene, MessageSegment.image(Path(la)))
 
 @yunshi.handle()
@@ -131,20 +131,20 @@ async def meme(bot: Bot, event: MessageEvent):
 
     la = os.listdir("data/pictures/amm")
     la = "data/pictures/amm/" + random.choice(la)
-    logger.info("执行运势查询")
+    logger1.info("执行运势查询")
     await bot.send_group_message(event.scene, MessageSegment.image(Path(la)))
 
 @TodatTarot.handle()
 async def tarotToday(bot: Bot, event: MessageEvent):
-    logger.info("获取今日塔罗")
+    logger1.info("获取今日塔罗")
     txt, img = tarotChoice()
-    logger.info("成功获取到今日塔罗")
+    logger1.info("成功获取到今日塔罗")
     await bot.send(event, txt)
     await bot.send_group_message(event.scene, MessageSegment.image(Path(img)))
 
 @colorAnime.handle()
 async def tarotToday(bot: Bot, event: MessageEvent):
-        logger.info("彩色小人，启动！")
+        logger1.info("彩色小人，启动！")
         c = random.choice(colorfulCharacterList)
         await bot.send_group_message(event.scene, MessageSegment.image(Path("data/colorfulAnimeCharacter/" + c)))
 
@@ -156,10 +156,10 @@ async def handle_group_message(bot: Bot, event: MessageEvent):
         return
     else:
         picNum = int(get_number(str(event.get_plaintext())))
-        logger.info("图片获取指令....数量：" + str(picNum))
+        logger1.info("图片获取指令....数量：" + str(picNum))
         if picNum < 10 and picNum > -1:
             for i in range(picNum):
-                logger.info("获取壁纸")
+                logger1.info("获取壁纸")
                 a = pic()
                 await bot.send_group_message(event.scene, MessageSegment.image(Path(a)))
         elif picNum == '':
@@ -167,7 +167,7 @@ async def handle_group_message(bot: Bot, event: MessageEvent):
             await bot.send_group_message(event.scene, MessageSegment.image(Path(a)))
         else:
             await bot.send(event, "数字超出限制")
-        logger.info("图片获取完成")
+        logger1.info("图片获取完成")
 
 
 # 整点正则
@@ -199,7 +199,7 @@ async def setuHelper(bot: Bot, event: MessageEvent):
         "r18", "")
     match1 = re.search(pattern1, text1)
     if match1:
-        logger.info("提取图片关键字。 数量: " + str(match1.group(1)) + " 关键字: " + match1.group(2))
+        logger1.info("提取图片关键字。 数量: " + str(match1.group(1)) + " 关键字: " + match1.group(2))
         data = {"tag": ""}
         if "r18" in str(event.get_plaintext()) or "色图" in str(event.get_plaintext()) or "涩图" in str(
                 event.get_plaintext()):
@@ -210,7 +210,7 @@ async def setuHelper(bot: Bot, event: MessageEvent):
         picData[event.senderUin] = []
         data["tag"] = match1.group(2)
         data["size"] = "regular"
-        logger.info("组装数据完成：" + str(data))
+        logger1.info("组装数据完成：" + str(data))
         a = int(match1.group(1))
         if int(match1.group(1)) > 6:
             a = 5
@@ -219,22 +219,22 @@ async def setuHelper(bot: Bot, event: MessageEvent):
             try:
                 path = await setuGet(data)
             except:
-                logger.error("涩图请求出错")
+                logger1.error("涩图请求出错")
                 await bot.send(event, "请求出错，请稍后再试")
                 return
-            logger.info("发送图片: " + path)
+            logger1.info("发送图片: " + path)
             try:
                 await bot.send_group_message(event.scene, MessageSegment.image(Path(path)))
-                logger.info("图片发送成功")
+                logger1.info("图片发送成功")
             except:
-                logger.error("图片发送失败")
+                logger1.error("图片发送失败")
                 await bot.send(event, path)
             '''try:
                 b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
                                         message_chain=MessageChain([" " , Image(url=path)]))
                 picData.get(event.senderUin).append(b1)
             except:
-                logger.error("出错，转为url文本")
+                logger1.error("出错，转为url文本")
                 b1 = ForwardMessageNode(sender_id=bot.qq, sender_name="Manyana",
                                         message_chain=MessageChain([" " , path]))
                 picData.get(event.senderUin).append(b1)
@@ -249,9 +249,9 @@ async def historyToday(bot: Bot, event: MessageEvent):
     string = str(event.get_plaintext())
     match = re.search(pattern, string)
     if match:
-        dataBack = await hisToday()
-        logger.info("获取历史上的今天")
-        logger.info(str(dataBack))
+        dataBack = await hisToday1()
+        logger1.info("获取历史上的今天")
+        logger1.info(str(dataBack))
         sendData = str(dataBack.get("result")).replace("[", " ").replace("{'year': '", "").replace("'}", "").replace(
             "]", "").replace("', 'title': '", " ").replace(",", "\n")
         await bot.send(event, sendData)
@@ -266,7 +266,7 @@ async def weather_query(bot: Bot, event: MessageEvent):
     if m:
         # 取出指令中的地名
         city = m.group(1)
-        logger.info("查询 " + city + " 天气")
+        logger1.info("查询 " + city + " 天气")
         await bot.send(event, '查询中……')
         # 发送天气消息
         await bot.send(event, await weatherQuery.querys(city, api_KEY))
@@ -275,26 +275,26 @@ async def weather_query(bot: Bot, event: MessageEvent):
 @newss.handle()
 async def newsToday(bot: Bot, event: MessageEvent):
 
-    logger.info("获取新闻")
+    logger1.info("获取新闻")
     path = await news()
-    logger.info("成功获取到今日新闻")
+    logger1.info("成功获取到今日新闻")
     await bot.send_group_message(event.scene, MessageSegment.image(Path(path)))
 
 
 @moyurenlili.handle()
 async def moyuToday(bot: Bot, event: MessageEvent):
 
-    logger.info("获取摸鱼人日历")
+    logger1.info("获取摸鱼人日历")
     path = await moyu()
-    logger.info("成功获取到摸鱼人日历")
+    logger1.info("成功获取到摸鱼人日历")
     await bot.send_group_message(event.scene, MessageSegment.image(Path(path)))
 
 
 @xingzuoQuery.handle()
 async def moyuToday(bot: Bot, event: MessageEvent):
-    logger.info("获取星座运势")
+    logger1.info("获取星座运势")
     path = await xingzuo()
-    logger.info("成功获取到星座运势")
+    logger1.info("成功获取到星座运势")
     await bot.send_group_message(event.scene, MessageSegment.image(Path(path)))
 
 
@@ -302,7 +302,7 @@ async def moyuToday(bot: Bot, event: MessageEvent):
 @jokes.handle()
 async def make_jokes(bot: Bot, event: MessageEvent):
 
-    x = str(event.get_plaintext()).strip()[1:-2]
+    x = str(event.get_plaintext()).strip()[0:-2]
     joke = get_joke(x)
     await bot.send(event, joke)
 
@@ -317,7 +317,7 @@ async def make_cp_mesg(bot: Bot, event: MessageEvent):
         text = 'エラーが発生しました。再入力してください'
         try:
             tex = '[JA]' + text + '[JA]'
-            logger.info("启动文本转语音：text: " + tex + " path: " + path[3:])
+            logger1.info("启动文本转语音：text: " + tex + " path: " + path[3:])
             await voiceGenerate({"text": tex, "out": path})
             await bot.send(event, Voice(path=path))
         except:
@@ -331,7 +331,7 @@ async def make_cp_mesg(bot: Bot, event: MessageEvent):
 async def NasaHelper(bot: Bot, event: MessageEvent):
     global data
 
-    logger.info(str(data.keys()))
+    logger1.info(str(data.keys()))
     if datetime.datetime.now().strftime('%Y-%m-%d') in data.keys():
         todayNasa = data.get(datetime.datetime.now().strftime('%Y-%m-%d'))
         path = todayNasa.get("path")
@@ -349,7 +349,7 @@ async def NasaHelper(bot: Bot, event: MessageEvent):
         }
         # Replace the key with your own
         dataa = {"api_key": nasa_api}
-        logger.info("发起nasa请求")
+        logger1.info("发起nasa请求")
         try:
             # 拼接url和参数
             url = "https://api.nasa.gov/planetary/apod?" + "&".join([f"{k}={v}" for k, v in dataa.items()])
@@ -357,8 +357,8 @@ async def NasaHelper(bot: Bot, event: MessageEvent):
                 # 用get方法发送请求
                 response = await client.get(url=url)
             # response = requests.post(url="https://saucenao.com/search.php", data=dataa, proxies=proxies)
-            logger.info("获取到结果" + str(response.json()))
-            # logger.info("下载缩略图")
+            logger1.info("获取到结果" + str(response.json()))
+            # logger1.info("下载缩略图")
             filename = await picDwn(response.json().get("url"),
                                     "data/pictures/nasa/" + response.json().get("date") + ".png")
             txta = await translate(response.json().get("explanation"), app_id=app_id, app_key=app_key, ori="en",
@@ -376,13 +376,13 @@ async def NasaHelper(bot: Bot, event: MessageEvent):
             #await bot.send(event, (Image(path=filename), txt))
 
         except:
-            logger.warning("获取每日天文图片失败")
+            logger1.warning("获取每日天文图片失败")
             await bot.send(event, "获取失败，请联系master检查代理或api_key是否可用")
 
 
 @operator.handle()
 async def arkGene(bot: Bot, event: MessageEvent):
-        logger.info("又有皱皮了，生成干员信息中.....")
+        logger1.info("又有皱皮了，生成干员信息中.....")
         o = arkOperator()
         o = o.replace("为生成", event.sendMemberName)
         await bot.send(event, o)
@@ -391,35 +391,35 @@ async def arkGene(bot: Bot, event: MessageEvent):
 @opStart.handle()
 async def genshin1(bot: Bot, event: MessageEvent):
 
-    logger.info("有原皮！获取抽签信息中....")
+    logger1.info("有原皮！获取抽签信息中....")
     o = genshinDraw()
-    logger.info("\n" + o)
+    logger1.info("\n" + o)
     await bot.send(event, o)
 
 
 @qiancao.handle()
 async def genshin1(bot: Bot, event: MessageEvent):
-    logger.info("获取浅草百签")
+    logger1.info("获取浅草百签")
     o = qianCao()
-    logger.info(o)
+    logger1.info(o)
     await bot.send(event, o)
 
 
 @poetrys.handle()
 async def NasaHelper(bot: Bot, event: MessageEvent):
 
-    logger.info("获取一篇诗经")
+    logger1.info("获取一篇诗经")
     ode = random.choice(odes.get("诗经"))
-    logger.info("\n" + ode)
+    logger1.info("\n" + ode)
     await bot.send(event, ode)
 
 
 @YiChou.handle()
 async def NasaHelper(bot: Bot, event: MessageEvent):
 
-    logger.info("获取卦象")
+    logger1.info("获取卦象")
     IChing1 = random.choice(IChing.get("六十四卦"))
-    logger.info("\n" + IChing1)
+    logger1.info("\n" + IChing1)
     await bot.send(event, IChing1)
 
 
@@ -427,11 +427,11 @@ async def NasaHelper(bot: Bot, event: MessageEvent):
 async def screenShoot(bot: Bot, event: MessageEvent):
     url=str(event.get_plaintext()).replace("截图#","")
     path = "data/pictures/cache/" + random_str() + ".png"
-    logger.info("接收网页截图任务url:" + url)
+    logger1.info("接收网页截图任务url:" + url)
     try:
         await screenshot_to_pdf_and_png(url, path)
     except:
-        logger.error("截图失败!")
+        logger1.error("截图失败!")
     await bot.send_group_message(event.scene, MessageSegment.image(Path(path)))
 
 
@@ -444,19 +444,19 @@ async def NasaHelper(bot: Bot, event: MessageEvent):
         except:
             await bot.send("未传递合法的prompt")
             return
-        logger.info("拆分获取prompt:" + str(ls))
+        logger1.info("拆分获取prompt:" + str(ls))
         try:
             url = "https://api.pearktrue.cn/api/stablediffusion/?prompt=" + str(ls) + "&model=normal"
             url = requests.get(url).json().get("imgurl")
         except:
-            logger.error("出错")
+            logger1.error("出错")
             await bot.send(event, "出错，请稍后再试")
             return
         path = "data/pictures/cache/" + random_str() + ".png"
         try:
             p = await sd(url, path)
         except:
-            logger.error("出错")
+            logger1.error("出错")
             await bot.send(event, "出错，请稍后再试")
             return
         await bot.send(event, Image(path=p), True)'''
@@ -464,39 +464,39 @@ async def NasaHelper(bot: Bot, event: MessageEvent):
 
 @honorYou.handle()
 async def jiangzhuang(bot: Bot, event: MessageEvent):
-    if str(event.get_plaintext()).startswith("/奖状"):
-        try:
-            t = str(event.get_plaintext()).replace("/奖状","").split("#")
-            url = "https://api.pearktrue.cn/api/certcommend/?name=" + t[0] + "&title=" + t[1] + "&classname=" + t[2]
 
-            p = await sd(url, "data/pictures/cache/" + random_str() + ".png")
-            await bot.send_group_message(event.scene, MessageSegment.image(Path(p)))
-        except:
-            await bot.send(event, "出错，格式请按照/奖状 孙笑川#天皇#阳光小学一年级2班")
+    try:
+        t = str(event.get_plaintext()).replace("/奖状","").split("#")
+        url = "https://api.pearktrue.cn/api/certcommend/?name=" + t[0] + "&title=" + t[1] + "&classname=" + t[2]
+
+        p = await sd(url, "data/pictures/cache/" + random_str() + ".png")
+        await bot.send_group_message(event.scene, MessageSegment.image(Path(p)))
+    except:
+        await bot.send(event, "出错，格式请按照/奖状 孙笑川#天皇#阳光小学一年级2班")
 
 
 @ark10.handle()
 async def moyuToday(bot: Bot, event: MessageEvent):
 
-    logger.info("获取方舟抽卡结果")
+    logger1.info("获取方舟抽卡结果")
     try:
         path = await arkGacha()
-        logger.info("成功获取到抽卡结果")
+        logger1.info("成功获取到抽卡结果")
         await bot.send_group_message(event.scene, MessageSegment.image(Path(path)))
     except:
-        logger.error("皱皮衮")
+        logger1.error("皱皮衮")
         await bot.send(event, "获取抽卡结果失败，请稍后再试")
 
 
 @starRailOrdor.handle()
 async def moyuToday(bot: Bot, event: MessageEvent):
 
-    logger.info("获取星铁抽卡结果")
+    logger1.info("获取星铁抽卡结果")
     try:
         path = await starRailGacha()
-        logger.info("成功获取到星铁抽卡结果")
+        logger1.info("成功获取到星铁抽卡结果")
         await bot.send_group_message(event.scene, MessageSegment.image(Path(path)))
     except:
-        logger.error("穹批衮")
+        logger1.error("穹批衮")
         await bot.send(event, "获取抽卡结果失败，请稍后再试")
 
